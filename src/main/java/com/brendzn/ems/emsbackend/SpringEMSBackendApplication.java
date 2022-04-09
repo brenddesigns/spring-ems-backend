6284466757dff4b58da47ec2f269157a35d8d3b2
brenddesigns <brenddesigns@hotmail.com>
2022-04-09 15:38:05 +1000

    Initial Commit + Project Structure

---
+++ src/main/java/com/brendzn/ems/emsbackend/SpringEMSBackendApplication.java
@@ -0,0 +1,12 @@
+package com.brendzn.ems.emsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEMSBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEMSBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brendzn/ems/emsbackend/SpringEMSBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendzn.ems.emsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEMSBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
